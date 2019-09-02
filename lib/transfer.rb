77d79246ffafef12fe060863ba876559f3b753d8
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-02 22:46:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,9 +17,9 @@ class Transfer
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
-  else
+  elsif !valid? && sender.balance < amount
     return "Transaction rejected. Please check your account balance."
   end 
-end
+ end
 
 end
