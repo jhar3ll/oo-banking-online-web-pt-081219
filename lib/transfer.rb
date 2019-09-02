fb1351944c08cda4d18d150d9f50600758c132ae
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-02 22:50:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if valid? && @status == "pending" && sender.balance >= amount 
+    if valid? && @status == "pending" && receiver.balance >= amount 
       sender.balance += amount
       receiver.balance -= amount
       @status = "complete"
